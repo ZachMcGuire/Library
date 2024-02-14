8e7c9128a730e5ecd10babec596d8ed46c3da5cb
ZachMcGuire <zach.mcguire09@gmail.com>
2024-02-14 07:36:34 -1000

    added styling

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const myLibrary = [];
+let newBook = [];
+
 
 const btn = document.querySelectorAll('.newBookBtn');
+
+const closeBtn = document.querySelectorAll('.closeForm');
 
 const inputTitle = document.querySelector('.titleInput');
 const inputAuthor = document.querySelector('.authorInput');
@@ -14,24 +18,11 @@ function Book (title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-};
+}
 
 Book.prototype.info = function () {
-    return `${this.title} by ${this.author}, has ${this.pages} pages, and ${this.read}`
-};
-
-function addBookToLibrary (newBook) {
-    // myLibrary.push(newBook)
-    myLibrary.push(hobbit);
-    myLibrary.push(EloquentJavaScript);
-};
-
-const hobbit = new Book('Hobbit', 'J.R.R. Tolken', 295, 'read');
-const EloquentJavaScript = new Book('Eloquent JavaScript', 'Marijn Haverbeke', 450, 'not read');
-
-
-addBookToLibrary();
-console.log(myLibrary)
+    return `${this.title} by ${this.author}, has ${this.pages} pages.`
+}
 
 function showForm () {
     btn.forEach (btn => {
@@ -40,7 +31,16 @@ function showForm () {
             form.removeAttribute('id');
         })
     })
-};
+}
+
+function closeForm () {
+    closeBtn.forEach (closeBtn => {
+        closeBtn.addEventListener('click', function() {
+            const form = document.querySelector('.formContainer');
+            form.setAttribute('id', 'hidden');
+        })
+    })
+}
 
 function getValue (selector) {
     const element = document.querySelector(selector).value;
@@ -52,15 +52,54 @@ function getInput () {
         inputBtn.addEventListener('click', function(e) {
             let title = getValue('.titleInput')
             let authors = getValue('.authorInput')
+            let pages = getValue('.pagesInput')
 
-            console.log(title)
-            console.log(authors)
+            let newBook = new Book(title, authors, pages)
 
-            console.log(typeof(title))
+            myLibrary.push(newBook);
+
+            displayBooks();
         })
     })
 }
 
+function addBookToLibrary () {
+    const book = new Book(title, authors, pages)
+    myLibrary.push(book)
+};
+
+function displayBooks() {
+    const displayContainer = document.querySelector('.bookView');
+    displayContainer.innerHTML = '';
+
+    myLibrary.forEach((Book, index) => {
+        const form = document.querySelector('.formContainer');
+        const bookElement = document.createElement('div');
+        bookElement.classList.add('book')
+        bookElement.innerHTML = `
+        <p class = "bookInfo">Title: ${Book.title}</p>
+        <p class = "bookInfo">Author: ${Book.author}</p>
+        <p class = "bookInfo">Pages: ${Book.pages}</p>
+        <button  class = "readBtn" onclick="toggleReadStatus(${index})">${Book.read}</button>
+        <button class = "removeBtn" onclick="removeBook(${index})">Remove</button>
+        `;
+        form.setAttribute('id', 'hidden');
+        displayContainer.appendChild(bookElement);
+    })
+}
+
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+    displayBooks();
+}
+
+function toggleReadStatus(index) {
+    const book = myLibrary[index];
+    book.read = (book.read === 'read') ? 'Not Read' : 'Read';
+    displayBooks();
+}
 
 showForm();
+closeForm();
 getInput();
+displayBooks();
